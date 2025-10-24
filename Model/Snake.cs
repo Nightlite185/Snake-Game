a688be14b15deda4050eae094b7249c233545ece
夜光 <cieplucharafal@gmail.com>
2025-10-24 23:26:19 +0200

    removed Head class and replaced with a simple flag in Segment class

--- Model/Snake.cs
+++ Model/Snake.cs
@@ -1,6 +1,6 @@
 ﻿namespace SnakeGame.Model
 {
-    public enum Direction { Left, Right, Up, Down }
+    public enum Direction { Up = 1, Right = 2, Down = 3, Left = 4}
     class Snake
     {
         private const int maxLength = 50; // this is a placeholder, to change later. It depends on the number of squares in the grid.
@@ -9,7 +9,7 @@
             ArgumentOutOfRangeException.ThrowIfLessThan(startingLength, 2); // cannot be less than two bc we are adding head first, outside of the loop.
             ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(startingLength, maxLength);
 
-            SnakeSegments.Add(new Head(startingCoords.X, startingCoords.Y, direction));
+            Body.Add(new SnakeSegment(startingCoords.X, startingCoords.Y, direction, isHead: true)); // adding head first
 
             for (int i = 1; i < startingLength - 1; i++)
             {
@@ -31,15 +31,15 @@
                     _ => startingCoords.X
                 };
 
-                SnakeSegments.Add(new Segment(nextX, nextY, direction));
+                Body.Add(new SnakeSegment(nextX, nextY, direction));
             }
             CurrentLength = startingLength;
         }
         
-        public int CurrentLength { get => SnakeSegments.Count; init {} }
-        public Head GetHead { get => (Head)SnakeSegments[0]; set {} }
-        public Segment GetTail { get => SnakeSegments.Last(); set{}}
-        public List<Segment> SnakeSegments { get; set; } = [];
+        public int CurrentLength { get => Body.Count; init {} }
+        public SnakeSegment Head { get => Body[0]; set {} }
+        public SnakeSegment Tail { get => Body.Last(); set{}}
+        public List<SnakeSegment> Body { get; set; } = [];
         public bool Alive { get; private set; } = true;
         
 
@@ -47,10 +47,10 @@
         {
             if (!Alive) throw new InvalidOperationException("can't eat if you're dead bro");
 
-            Segment tail = GetTail; // assigning tail to new var for reusing, its a waste to call Last() more than once.
+            SnakeSegment tail = Tail; // assigning tail to new var for reusing, its a waste to call Last() more than once.
 
             // calculating new segment's coords after growing
-            Segment newSegment = tail.Facing switch
+            SnakeSegment newSegment = tail.Facing switch
             {
                 Direction.Left => new(tail.X + 1, tail.Y, tail.Facing),
                 Direction.Right => new(tail.X - 1, tail.Y, tail.Facing),
@@ -60,22 +60,23 @@
                 _ => throw new Exception("Sth went wrong, tail's Direction enum was neither of it's values - probably null")
             };
 
-            SnakeSegments.Add(newSegment);
+            Body.Add(newSegment);
         }
         public void Die()
         {
             Alive = false;
-            SnakeSegments.Clear();
+            Body.Clear();
             OnDeath?.Invoke(this);
         }
         public event Action<Snake>? OnDeath;
-        public class Segment(int x, int y, Direction direction)
+        public class SnakeSegment(int x, int y, Direction direction, bool isHead = false)
         {
             public int X { get; set; } = x;
             public int Y { get; set; } = y;
             public Direction Facing { get; set; } = direction;
             public bool IsBent { get; set; } = false;
+            public bool IsHead { get; set; } = isHead;
         }
-        public class Head(int x, int y, Direction direction) : Segment(x, y, direction); // head cannot be bent so maybe do sth ab it in the future.
+        
     }                                                                                    // instead of blindly extending the Head class with Segment.
 }
