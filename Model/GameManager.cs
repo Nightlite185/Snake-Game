1e34a01e915e44e51d9b7364aebf47812777280f
夜光 <cieplucharafal@gmail.com>
2025-10-24 23:25:26 +0200

    added MoveSnake method and cleaned code a bit.

--- Model/GameManager.cs
+++ Model/GameManager.cs
@@ -2,17 +2,36 @@
 {
     public class GameManager()
     {
-        // grid constants definitions
+        #region const definitions
+        // grid
         private const int gridRows = 400;
         private const int gridColumns = 400;
 
-        // snake constants definitions
+        // snake
         private const int StartingLength = 3;
         private const Direction StartingDirection = Direction.Up;
         private static readonly (int X, int Y) startingCoords = (50, 50);
+        #endregion
 
         //constructing game objects
-        private GameGrid Grid { get; init; } = new(gridRows, gridColumns);
-        private Snake Snake { get; init; } = new(StartingLength, StartingDirection, startingCoords);
+        public GameGrid Grid { get; init; } = new(gridRows, gridColumns);
+        private Snake Snake { get; set; } = new(StartingLength, StartingDirection, startingCoords);
+        private GameState GameState { get; init; } = new();
+        
+        public void MoveSnake(Direction newDirection)
+        {
+            if (Math.Abs(Snake.Head.Facing - newDirection) == 2) // if u turn in opposite direction - 死ねええええ!!!!!
+            {
+                Snake.Die();
+                GameState.Lose();
+            }
+
+            var tail = Snake.Tail; // save tail
+            Snake.Body.RemoveAt(Snake.CurrentLength - 1); // pop it from body
+
+            Snake.Head.IsHead = false; // update old head's flag
+            Snake.Body.Insert(0, tail); // and glue it to the front
+            Snake.Head.IsHead = true; // and update new head's (old tail's) flag
+        }
     }
 }
