6f5dce0e15b230cc13b54e5cdf333049b0be8216
夜光 <cieplucharafal@gmail.com>
2025-10-29 21:36:31 +0100

    added more skeleton of viewmodel

--- ViewModel/GameViewModel.cs
+++ ViewModel/GameViewModel.cs
@@ -1,9 +1,24 @@
 using System.ComponentModel;
+using System.Windows.Input;
+using SnakeGame.Commands;
+using SnakeGame.Model;
 
 namespace SnakeGame.ViewModel
 {
-    public class GameViewModel() : INotifyPropertyChanged
-    {
+    public class GameViewModel : INotifyPropertyChanged
+    { 
+        public GameViewModel()
+        {
+            this.gameManager = new();
+            StartGameCommand = new RelayCommand(gameManager.RunGameAsync);
+        }
+        private const int GridRows = 20;
+        private const int GridColumns = 20;
+        private readonly GameManager gameManager;
+
+
+        public ICommand StartGameCommand { get; }
+
         public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
