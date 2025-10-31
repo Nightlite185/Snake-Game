c842daffe9245fd9b85c76dc8b55b5bc00d3bfe9
夜光 <cieplucharafal@gmail.com>
2025-10-31 18:00:03 +0100

    made methods into blocks instead of expr. bc its easier to invoke custom events

--- Model/GameState.cs
+++ Model/GameState.cs
@@ -3,36 +3,57 @@
     public enum GameStates {Lost, Won, NotStarted, Paused, Running};
     public class GameState()
     {
-        public event Action<GameStates>? OnStateChange;
+        public event EventHandler<GameStates>? OnStateChange;
+        public event Action? OnGameStarted;
         public GameStates CurrentState { get; private set; } = GameStates.NotStarted;
         private GameStates ChangeState(GameStates newState) // separate method to keep it clean and invoke event in the same time.
-        {
-            OnStateChange?.Invoke(newState);
+        {                                                   // kinda useless tho since its too universal..
+            OnStateChange?.Invoke(this, newState);
             return newState;
         }
 
-        public void Pause() => CurrentState = (CurrentState != GameStates.Running)
-            ? throw new InvalidOperationException($"cannot pause a game that is {CurrentState}.")
-            : ChangeState(GameStates.Paused);
-            
-        public void Resume() => CurrentState = (CurrentState != GameStates.Paused)
-            ? throw new InvalidOperationException($"cannot resume a game that is {CurrentState}.") 
-            : ChangeState(GameStates.Running);
+        public void Pause()
+        {
+            CurrentState = (CurrentState != GameStates.Running)
+                ? throw new InvalidOperationException($"cannot pause a game that is {CurrentState}.")
+                : GameStates.Paused;
+        }
 
-        public void Start() => CurrentState = (CurrentState != GameStates.NotStarted)
-            ? throw new InvalidOperationException($"cannot start a game that is {CurrentState}.")
-            : ChangeState(GameStates.Running);
+        public void Resume()
+        {
+            CurrentState = (CurrentState != GameStates.Paused)
+                ? throw new InvalidOperationException($"cannot resume a game that is {CurrentState}.")
+                : GameStates.Running;
+        }
 
-        public void Lose() => CurrentState = (CurrentState != GameStates.Running)
-            ? throw new InvalidOperationException($"cannot lose a game that is {CurrentState}.")
-            : ChangeState(GameStates.Lost);
+        public void Start()
+        {
+            CurrentState = (CurrentState != GameStates.NotStarted)
+                ? throw new InvalidOperationException($"cannot start a game that is {CurrentState}.")
+                : GameStates.Running;
 
-        public void Win() => CurrentState = (CurrentState != GameStates.Running)
-            ? throw new InvalidOperationException($"cannot win a game that is {CurrentState}.")
-            : ChangeState(GameStates.Won);
+            OnGameStarted?.Invoke();
+        }
 
-        public void Reset() => CurrentState = (CurrentState == GameStates.NotStarted)
-            ? throw new InvalidOperationException($"cannot restart a game that is {CurrentState}.")
-            : ChangeState(GameStates.NotStarted);
+        public void Lose()
+        {
+            CurrentState = (CurrentState != GameStates.Running)
+                ? throw new InvalidOperationException($"cannot lose a game that is {CurrentState}.")
+                : GameStates.Lost;
+        }
+
+        public void Win()
+        {
+            CurrentState = (CurrentState != GameStates.Running)
+                ? throw new InvalidOperationException($"cannot win a game that is {CurrentState}.")
+                : GameStates.Won;
+        }
+
+        public void Reset()
+        {
+            CurrentState = (CurrentState == GameStates.NotStarted)
+                ? throw new InvalidOperationException($"cannot restart a game that is {CurrentState}.")
+                : GameStates.NotStarted;
+        }
     }
 }
