b0f0a66da5803f0719b198ae5089af5dd9d1f82f
夜光 <cieplucharafal@gmail.com>
2025-11-03 00:03:47 +0100

    implemented IEquatable in Coords struct for debugging

--- Model/Coords.cs
+++ Model/Coords.cs
@@ -1,9 +1,13 @@
 namespace SnakeGame.Model
 {
-    public struct Coords(int row, int col)
+    public struct Coords(int row, int col) : IEquatable<Coords>
     {
         public int Row { get; set; } = row;
         public int Col { get; set; } = col;
+
+        public readonly bool Equals(Coords other)
+            => this.Row == other.Row && this.Col == other.Col;
+
         public override readonly string ToString() 
             => $"Row = {Row}, Col = {Col}";
     }
