d89846bc8af7565190323ec1ad7a5e2cf1f0ebb0
夜光 <cieplucharafal@gmail.com>
2025-11-17 23:19:14 +0100

    implemented all the "save", "discard", and "to default" commands

--- ViewModel/SettingsViewModel.cs
+++ ViewModel/SettingsViewModel.cs
@@ -1,35 +1,78 @@
+using SnakeGame.Helpers;
 using SnakeGame.Model;
 
 namespace SnakeGame.ViewModel
 {
     public class SettingsViewModel
     {
-        public SettingsViewModel(Settings settingsCopy)
+        private bool IsChanged
+        { 
+            get; 
+            set
+            {
+                field = value;
+
+                SaveChangesCommand.ScreamCanExecuteChanged();
+                DiscardChangesCommand.ScreamCanExecuteChanged();
+            } 
+        }
+        private bool IsDefault
+        { 
+            get; 
+            set
+            {
+                field = value;
+                ResetToDefaultCommand.ScreamCanExecuteChanged();    
+            } 
+        }
+        private Settings draftSettings;
+        private Settings OGSettingsRef;
+        public SettingsViewModel(Settings settings)
         {
-            this.settingsCopy = settingsCopy;
+            this.draftSettings = settings.DeepClone();
+            this.OGSettingsRef = settings;
+
             #region ICommands
-            //SaveChangesCommand = new RelayCommand(
-            //    execute:
-            //    canExecute:
-            //);
+            SaveChangesCommand = new RelayCommand(
+                execute: () =>
+                {
+                    this.OGSettingsRef = draftSettings;
+                    IsChanged = false;
+                    
+                    if (IsDefault)
+                        IsDefault = false;
+                },
+                canExecute: () => IsChanged
+            );
+            
+            ResetToDefaultCommand = new RelayCommand(
+                execute: () =>
+                {
+                    // TO DO:: maybe open some pop-up like "you sure u wanna reset???"
+                    draftSettings.ToDefault();
+                    this.OGSettingsRef = draftSettings;
 
-            //DiscardChangesCommand = new RelayCommand(
-            //    execute: 
-            //    canExecute:
-            //)
+                    IsChanged = false;
+                    IsDefault = true;
+                },
+                canExecute: () => !IsDefault
+            );
 
-            //ResetToDefaultCommand = new RelayCommand(
-            //    execute: 
-            //    canExecute:
-            //)
+            DiscardChangesCommand = new RelayCommand(
+                execute: () =>
+                {
+                    draftSettings = OGSettingsRef;
+                    IsChanged = false;
+                },
+                canExecute: () => IsChanged
+            );
             #endregion
         }
-        private readonly Settings settingsCopy;
         
         #region ICommands
-        //public ICommand SaveChangesCommand { get; }
-        //public ICommand DiscardChangesCommand { get; }
-        //public ICommand ResetToDefaultCommand { get; }
+        public RelayCommand SaveChangesCommand { get; }
+        public RelayCommand DiscardChangesCommand { get; }
+        public RelayCommand ResetToDefaultCommand { get; }
         #endregion
     }
 }
