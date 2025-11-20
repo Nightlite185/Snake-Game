8ad2853023b638bee792a6a64012332d08ecef46
夜光 <cieplucharafal@gmail.com>
2025-11-20 23:23:02 +0100

    properly getting slider's thumb and hooking events to it.

--- OptionsWindow.xaml.cs
+++ OptionsWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using SnakeGame.Model;
 using SnakeGame.ViewModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace SnakeGame
 {
@@ -10,10 +12,24 @@ namespace SnakeGame
         public OptionsWindow(Settings OGSettingsRef)
         {
             SetVM = new SettingsViewModel(OGSettingsRef);
-            
+
             this.DataContext = SetVM;
 
             InitializeComponent();
+
+            SnakeLengthSlider.ManipulationCompleted += (_, _) => SetVM.IsChanged = true;
+            SnakeLengthSlider.DragOver += (_, _) => SetVM.IsChanged = true;
+
+            HookEventsToThumb(SnakeLengthSlider);
+        }
+        private void HookEventsToThumb(Slider s)
+        {
+            s.ApplyTemplate();
+
+            var thumb = ((s.Template.FindName("PART_Track", s) as Track)?.Thumb) 
+                ?? throw new Exception("thumb not found");
+
+            thumb.DragCompleted += (_, _) => SetVM.IsChanged = true;
         }
     }
 }
