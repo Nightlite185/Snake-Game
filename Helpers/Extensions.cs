dc01974322a96bcef8bbcaaeeaf6481d78990b22
夜光 <cieplucharafal@gmail.com>
2025-11-22 20:04:46 +0100

    helper ext. method to get the bound property's name

---
+++ Helpers/Extensions.cs
@@ -0,0 +1,17 @@
+using System.Windows;
+
+namespace SnakeGame.Helpers
+{
+    public static class Helpers
+    {
+        public static string GetBindingPath(this FrameworkElement el, DependencyProperty dp)
+        {
+            var binding = el.GetBindingExpression(dp);
+            
+            string propName = binding?.ParentBinding.Path.Path 
+                ?? throw new Exception("No binding path found, make sure you provided one in xaml.");
+
+            return propName.Split('.').Last();
+        } 
+    }
+}
